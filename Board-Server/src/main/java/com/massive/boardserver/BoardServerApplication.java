442daeb0683d9d038e13514c5fd6f7d8424ccf3a
SEUNGREEJEON <jws08219@naver.com>
2025-05-31 21:18:31 +0900

    ENH: issue/2 유저 api 개발 - JDBC Mysql 연동 - Mybatis 매퍼 설정 - 세션을 활용한 로그인 기능 개발 - 비민번호 암호화 개발 - 데모 및 테스트

---
+++ Board-Server/src/main/java/com/massive/boardserver/BoardServerApplication.java
@@ -0,0 +1,14 @@
+package com.massive.boardserver;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardServerApplication.class, args);
+	}
+
+}
