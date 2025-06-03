e0743a84d40697158081516f455b0e9adca094aa
SEUNGREEJEON <jws08219@naver.com>
2025-06-03 12:10:55 +0900

    feat: 로깅,예외처리 개발 로깅 레벨 설정 예외처리 전략 탐구 공통 예외처리 개발 데모 및 테스트

--- Board-Server/src/main/java/com/massive/boardserver/service/impl/CategoryServiceImpl.java
+++ Board-Server/src/main/java/com/massive/boardserver/service/impl/CategoryServiceImpl.java
@@ -17,7 +17,15 @@ public class CategoryServiceImpl implements CategoryService {
 	@Override
 	public void register(String accountId, CategoryDTO categoryDTO) {
 		if (accountId != null) {
-			categoryMapper.register(categoryDTO);
+
+			try {
+
+				categoryMapper.register(categoryDTO);
+			} catch (RuntimeException e) {
+				log.error("register ERROR!", categoryDTO);
+				throw new RuntimeException("register ERROR! 상품 카테고리 등록 메서드를 확인해주세요\n" + "Params : " + categoryDTO);
+			}
+
 		} else {
 			log.error("register ERROR! {}", categoryDTO);
 			throw new RuntimeException("register ERROR! 상품 카테고리 등록 메서드를 확인해주세요\n" + "Params : " + categoryDTO);
